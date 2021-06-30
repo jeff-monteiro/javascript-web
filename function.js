ef232929dd05e32f102850ecc1ea3b1f6a18a50a
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-30 00:20:00 -0300

    functions

---
+++ function.js
@@ -0,0 +1,12 @@
+function indicadorIdade(nomePessoa, anoDeNascimento) {
+  
+  nomePessoa = 'Jefferson'
+  anoDeNascimento = 1995
+  var idade = 30
+
+  if(idade > 18){
+    return 2021 - anoDeNascimento
+  }
+  console.log(indicadorIdade())
+  
+}
