846b7ec806e238b42dcba78efb09660791bacba0
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-30 08:37:19 -0300

    ExemplosFunctions

--- function.js
+++ function.js
@@ -11,11 +11,11 @@ function verificarIdade(nomePessoa, anoDeNascimento) {
 }
 var nome = 'Joelma'
 var nascimento = 2002
-console.log(verificarIdade(nome, nascimento))
+console.log(verificarIdade(nome, nascimento))//Utilizando função atribuindo variáveis e valores as variáveis.
 
-console.log(verificarIdade("Cassiano", 1988))
+console.log(verificarIdade("Cassiano", 1988))//Atribuindo valores implicitamente na função
 
 nome = "John"
 nascimento = 1978
-var texto = verificarIdade(nome, nascimento)
+var texto = verificarIdade(nome, nascimento)//Atribuindo a função a uma outra variável.
 console.log(texto)
