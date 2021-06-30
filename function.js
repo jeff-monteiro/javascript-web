2e2d573a662bd4633b4ca5c8fa666e12492addc3
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-30 08:34:16 -0300

    ExemplosFunctions

--- function.js
+++ function.js
@@ -1,15 +1,21 @@
 function verificarIdade(nomePessoa, anoDeNascimento) {
   
-  
-  var idade = 2018 - anoDeNascimento
+  var idade = 2021 - anoDeNascimento
 
   if(idade >= 18){
     return nomePessoa + " é maior de idade."
   }else{
     return nomePessoa + " tem menos de 18 anos."
   }
-   
+
 }
 var nome = 'Joelma'
 var nascimento = 2002
 console.log(verificarIdade(nome, nascimento))
+
+console.log(verificarIdade("Cassiano", 1988))
+
+nome = "John"
+nascimento = 1978
+var texto = verificarIdade(nome, nascimento)
+console.log(texto)
