ff4545cefc0c71e0a60b233456585379d207332a
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-08-11 08:54:00 -0300

    TesteSoma

--- fundamentos-js/numerosAlgunsCuidados.js
+++ fundamentos-js/numerosAlgunsCuidados.js
@@ -1,2 +1,8 @@
 console.log(7 / 0)
 console.log('Olá mundo!')
+function somar(numero1, numero2){
+  return numero1 + numero2;
+}
+let numero1 = 15
+let numero2 = 16
+console.log(somar(numero1, numero2))
