53dd4176c5904d2738ce50ceeaa568245ff37ba1
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-08-11 09:18:35 -0300

    NewTeste

--- fundamentos-js/numerosAlgunsCuidados.js
+++ fundamentos-js/numerosAlgunsCuidados.js
@@ -5,4 +5,5 @@ function somar(numero1, numero2){
 }
 let numero1 = 15
 let numero2 = 16
-console.log(somar(numero1, numero2))
+console.log('O valor da soma dos valores é: '+ somar(numero1, numero2))
+console.log('Deu certo')
