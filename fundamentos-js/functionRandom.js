5315137b333f32c042aab734f3d5e871fbec2efd
Jefferson <jeffmonteiro61@icloud.com>
2021-08-19 22:42:36 -0300

    functionRandom

---
+++ fundamentos-js/functionRandom.js
@@ -0,0 +1,5 @@
+const frutas = ['Abacaxi', 'Abacate', 'Manga', 'Maça', 'Pera', 'Pitomba', 'Tamarindo']
+function sortearFruta(){
+  const indice = Math.floor(Math.random() * frutas.length)
+  const 
+}
