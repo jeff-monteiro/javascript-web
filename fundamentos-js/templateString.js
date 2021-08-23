e784fe738ae709f5ce938ec45dacc129b1861e2f
Jefferson <jeffmonteiro61@icloud.com>
2021-08-23 12:07:51 -0300

    templateString

---
+++ fundamentos-js/templateString.js
@@ -0,0 +1,12 @@
+const nome = 'Rebeca'
+const concatenacao = 'Olá ' + nome + '!'
+const templateExample = `Olá ${nome}!`
+
+console.log(concatenacao, templateExample)
+
+//expressions
+console.log(`1 + 1 = ${1 + 1}`)
+
+//Arrow Function
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('Cuidado')}!`) 
