e6e0a023990930e92ab727f543a330a1ef940397
Jefferson <jeffmonteiro61@icloud.com>
2021-08-28 16:58:41 -0300

    null&Undefined

--- fundamentos-js/nullUndefined.js
+++ fundamentos-js/nullUndefined.js
@@ -4,4 +4,11 @@ console.log(valor)
 
 valor = null //Ausência de valor
 console.log(valor)
-console.log(valor.toString())
+// console.log(valor.toString()) //Erro!
+
+const produto = {} // Objeto vazio
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.85
+console.log(produto)
