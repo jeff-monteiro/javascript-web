9d8aa90014ba37f9d6332bc2db141d0ca66cd9d2
Jefferson <jeffmonteiro61@icloud.com>
2021-09-08 19:30:00 -0300

    hoisting

---
+++ fundamentos-js/hoisting.js
@@ -0,0 +1,10 @@
+// teste = () => {//Hoisting acontece quando utilizamos a palavra VAR
+//   console.log('a = ', a)
+//   var a = 2
+//   console.log('a = ', a)
+// }
+// teste()
+
+console.log('b = ', b)
+let b = 4
+console.log('b = ', b)
