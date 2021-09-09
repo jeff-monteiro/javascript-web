70be86ea153ebd125373d146f57e61dc148f3ce3
Jefferson <jeffmonteiro61@icloud.com>
2021-09-08 23:19:13 -0300

    Atribuiçao

--- fundamentos-js/notacaoPonto.js
+++ fundamentos-js/notacaoPonto.js
@@ -6,3 +6,19 @@ obj1.nome = 'Bola'
 
 console.log(obj1)
 console.log(obj1.nome)
+
+function Obj(nome){
+  this.nome = nome
+}
+function Obj2(sobrenome){
+  this.sobrenome = function(){
+    console.log('Monteiro')
+  }
+}
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+const obj4 = new Obj2()
+console.log(obj2.nome)
+console.log(obj3.nome)
+obj4.sobrenome()
