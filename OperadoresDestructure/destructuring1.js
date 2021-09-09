28ef1e90a67be79a5ff7952b56e1ca50dbb7d137
Jefferson <jeffmonteiro61@icloud.com>
2021-09-08 23:43:53 -0300

    Destructure#01

---
+++ OperadoresDestructure/destructuring1.js
@@ -0,0 +1,17 @@
+const pessoa = {
+  nome: 'Karoliny',
+  idade: 22,
+  endereco: {
+    logradouro: 'Rua Livreiro',
+    numero: 40
+  }
+}
+// console.log(pessoa.endereco.logradouro)
+const { nome, idade } = pessoa // Destructuring#01
+console.log(nome, idade)
+
+const { nome: n, idade: i} = pessoa // Alterando o nome da variável
+console.log(n, i)
+
+const { sobrenome, bemHumorada = true} = pessoa //Caso queira remover de dentro de um objeto que nunca existiu
+console.log(sobrenome, bemHumorada)
