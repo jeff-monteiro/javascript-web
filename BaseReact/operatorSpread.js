f12f64cfa80a15f73512762ac6fdb9c9503bfd7e
Jefferson <jeffmonteiro61@icloud.com>
2021-10-12 15:53:36 -0300

    OperatorSpread

--- BaseReact/operatorSpread.js
+++ BaseReact/operatorSpread.js
@@ -1,7 +1,7 @@
 const livros1 = ['CSS3', 'Javascript', 'PHP']
 const livros2 = ['HTML', 'React']
-//const precos = [70, 30, 90, 100, 10]
+const precos = [70, 30, 90, 100, 10]
 
-let livros = [...livros1, ...livros2]
+let livros = [...livros1, ...livros2, ...precos]
 
 console.log(livros)
