ef50bc266e8aeded89645c108d59b6338c5df3de
Jefferson <jeffmonteiro61@icloud.com>
2021-10-14 22:37:39 -0300

    destructurin2

--- BaseReact/operatorSpread.js
+++ BaseReact/operatorSpread.js
@@ -2,6 +2,8 @@ const livros1 = ['CSS3', 'Javascript', 'PHP']
 const livros2 = ['HTML', 'React']
 const precos = [70, 30, 90, 100, 10]
 
-let livros = [...livros1, ...livros2, ...precos]//Utilizando operador SPREAD
+let livros = [...livros1, ...livros2]//Utilizando operador SPREAD
+let precoMaximo1 = Math.max(...precos)
 
 console.log(livros)
+console.log(precoMaximo1)
