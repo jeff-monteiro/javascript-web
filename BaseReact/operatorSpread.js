50a68643de48a6e23e3f89f84713d4bc8cd89edf
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2021-12-16 10:57:47 -0300

    spreadOperator

--- BaseReact/operatorSpread.js
+++ BaseReact/operatorSpread.js
@@ -1,9 +1,11 @@
-const livros1 = [ 'CSS3' , 'Javascript' , 'PHP' ]
-const livros2 = [ 'HTML' , 'React' ]
+//const livros1 = [ 'CSS3' , 'Javascript' , 'PHP' ]
+//const livros2 = [ 'HTML' , 'React' ]
 const precos = [70, 30, 90, 100, 10]
 
-let livros = [...livros1, ...livros2]//Utilizando operador SPREAD.
+//let livros = [...livros1, ...livros2]//Utilizando operador SPREAD.
 let precoMaximo1 = Math.max(...precos)//Atribuindo um array como parâmetro ao operador SPREAD.
+let precoMinimo1 = Math.min(...precos)
 
-console.log([livros])
+//console.log([livros])
 console.log(precoMaximo1)
+console.log(precoMinimo1)
